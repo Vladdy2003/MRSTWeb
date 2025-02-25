49c2260ab013e8a09c191c56dd070ec24af66bc7
Vladislav <vladopr20@gmail.com>
2025-02-25 12:05:52 +0200

    New aspect

---
+++ EvenimentMD/Controllers/LogInController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EvenimentMD.Controllers
+{
+    public class LogInController : Controller
+    {
+        // GET: LogIn
+        public ActionResult LogInIndex()
+        {
+            return View();
+        }
+    }
+}
